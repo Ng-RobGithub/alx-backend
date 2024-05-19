ee75fca6f92cdfee2607b85a3919a8042447d652
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-20 00:44:33 +0100

    backend pagination task3

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -4,7 +4,7 @@ Deletion-resilient hypermedia pagination
 """
 
 import csv
-from typing import List, Dict, Any
+from typing import List
 
 
 class Server:
@@ -21,57 +21,24 @@ def __init__(self):
 def dataset(self) -> List[List]:
     """Cached dataset
     """
-
     if self.__dataset is None:
         with open(self.DATA_FILE) as f:
             reader = csv.reader(f)
             dataset = [row for row in reader]
-            self.__dataset = dataset[1:]  # Skip the header row
+            self.__dataset = dataset[1:]
 
-        return self.__dataset
+            return self.__dataset
 
 
-def indexed_dataset(self) -> Dict[int, List]:
+def indexed_dataset(self) -> dict[int, List]:
     """Dataset indexed by sorting position, starting at 0
     """
     if self.__indexed_dataset is None:
         dataset = self.dataset()
+        truncated_dataset = dataset[:1000]
         self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
         return self.__indexed_dataset
 
 
-def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict[str, Any]:
-    """
-    Get a page of the dataset with a resilient index to deletions.
-
-    Args:
-
-    index (int): The start index of the return page.
-    page_size (int): The number of items per page.
-
-    Returns:
-    Dict[str, Any]: A dictionary containing the page data and
-    hypermedia metadata.
-    """
-    assert isinstance(index, int) and index >= 0
-    assert isinstance(page_size, int) and page_size > 0
-
-    indexed_data = self.indexed_dataset()
-    dataset_size = len(indexed_data)
-    assert index < dataset_size
-
-    data = []
-    current_index = index
-    while len(data) < page_size and current_index < dataset_size:
-        if current_index in indexed_data:
-            data.append(indexed_data[current_index])
-            current_index += 1
-
-        next_index = current_index if current_index < dataset_size else None
-
-        return {
-            "index": index,
-            "next_index": next_index,
-            "page_size": len(data),
-            "data": data
-        }
+def get_hyper_index(self, index: int = None, page_size: int = 10) -> dict:
+    pass
