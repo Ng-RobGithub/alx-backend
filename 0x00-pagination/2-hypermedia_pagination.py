0d07f6e7f063c9ca6edd125af380fd5ee859fd01
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-20 22:03:14 +0100

    hyper pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
-"""
-Hypermedia Pagination Module
+"""Hypermedia pagination sample.
 """
 import csv
 import math
-from typing import List, Dict
+from typing import Dict, List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
 
 
 class Server:
@@ -14,6 +21,8 @@ class Server:
 
 
 def __init__(self):
+    """Initializes a new Server instance.
+    """
     self.__dataset = None
 
 
@@ -26,68 +35,35 @@ def dataset(self) -> List[List]:
             dataset = [row for row in reader]
             self.__dataset = dataset[1:]
 
-            return self.__dataset
+        return self.__dataset
 
 
 def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-    """Get a page of the dataset.
-
-    Args:
-    page (int): The page number to retrieve. Default is 1.
-    page_size (int): The number of items per page. Default is 10.
-
-    Returns:
-    List[List]: The requested page of the dataset.
-    An empty list if the page is out of range.
+    """Retrieves a page of data.
     """
-    assert isinstance(page, int) and page > 0
-    "Page must be a positive integer"
-    assert isinstance(page_size, int) and page_size > 0
-    "Page size must be a positive integer"
-
-    dataset = self.dataset()
-    total_items = len(dataset)
-    total_pages = math.ceil(total_items / page_size)
-
-    if page < 1 or page > total_pages:
+    assert isinstance(page, int) and type(page_size, int)
+    assert page > 0 and page_size > 0
+    start, end = index_range(page, page_size)
+    data = self.dataset()
+    if start > len(data):
         return []
 
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return dataset[start_index:end_index]
+    return data[start:end]
 
 
 def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-    """Get a page of the dataset with hypermedia pagination.
-
-    Args:
-    page (int): The page number to retrieve. Default is 1.
-
-    page_size (int): The number of items per page. Default is 10.
-
-    Returns:
-
-    Dict: A dictionary containing the following key-value pairs:
-    page_size: the length of the returned dataset page
-    page: the current page number
-    data: the dataset page (equivalent to return from previous task)
-    next_page: number of the next page, None if no next page
-    prev_page: number of the previous page, None if no previous page
-    total_pages: the total number of pages in the dataset as an integer
+    """Retrieves information about a page.
     """
-    dataset = self.get_page(page, page_size)
 
-    total_items = len(self.dataset())
-    total_pages = math.ceil(total_items / page_size)
-
-    next_page = page + 1 if page < total_pages else None
-    prev_page = page - 1 if page > 1 else None
-
-    return {
-        'page_size': len(dataset),
+    page_data = self.get_page(page, page_size)
+    start, end = index_range(page, page_size)
+    total_pages = math.ceil(len(self.__dataset) / page_size)
+    page_info = {
+        'page_size': len(page_data),
         'page': page,
-        'data': dataset,
-        'next_page': next_page,
-        'prev_page': prev_page,
-        'total_pages': total_pages
+        'data': page_data,
+        'next_page': page + 1 if end < len(self.__dataset) else None,
+        'prev_page': page - 1 if start > 0 else None,
+        'total_pages': total_pages,
         }
+    return page_info
