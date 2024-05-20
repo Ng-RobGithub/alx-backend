cfea88badd285ed10738f0e2027bf6134e32e5e9
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-20 22:37:12 +0100

    delete hyper pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
+"""Deletion-resilient hypermedia pagination
 """
-Deletion-resilient hypermedia pagination
-"""
-
 import csv
-from typing import List
+from typing import Dict, List
 
 
 class Server:
@@ -14,6 +12,8 @@ class Server:
 
 
 def __init__(self):
+    """Initializes a new Server instance.
+    """
     self.__dataset = None
     self.__indexed_dataset = None
 
@@ -30,15 +30,38 @@ def dataset(self) -> List[List]:
             return self.__dataset
 
 
-def indexed_dataset(self) -> dict[int, List]:
+def indexed_dataset(self) -> Dict[int, List]:
     """Dataset indexed by sorting position, starting at 0
     """
+
     if self.__indexed_dataset is None:
         dataset = self.dataset()
-        truncated_dataset = dataset[:1000]
-        self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
-        return self.__indexed_dataset
+    self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
+    return self.__indexed_dataset
 
 
-def get_hyper_index(self, index: int = None, page_size: int = 10) -> dict:
-    pass
+def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+    """Retrieves info about a page from a given index and with a
+    specified size.
+    """
+    data = self.indexed_dataset()
+    assert index is not None and index >= 0 and index <= max(data.keys())
+    page_data = []
+    data_count = 0
+    next_index = None
+    start = index if index else 0
+    for i, item in data.items():
+        if i >= start and data_count < page_size:
+            page_data.append(item)
+            data_count += 1
+            continue
+        if data_count == page_size:
+            next_index = i
+            break
+        page_info = {
+            'index': index,
+            'next_index': next_index,
+            'page_size': len(page_data),
+            'data': page_data,
+            }
+        return page_info
