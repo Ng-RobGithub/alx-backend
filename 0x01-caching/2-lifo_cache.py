5a0ab2a744db0983160c8da508d4de05f26cdcc7
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-21 22:07:31 +0100

    LIFOCache class

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,24 +1,53 @@
-#!/usr/bin/python3
-""" Python caching systems """
+#!/usr/bin/env python3
+"""Last-In First-Out caching module.
+"""
+from collections import OrderedDict
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ LIFO caching system """
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a LIFO
+    removal mechanism when the limit is reached.
+    """
+
     def __init__(self):
-        """ Initialize class instance. """
+        """Initializes the cache.
+
+        Uses an OrderedDict to maintain the order of insertion.
+        """
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item in the cache """
-        if key and item:
-            self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            self.cache_data.pop(self.last_item)
-            print('DISCARD:', self.last_item)
-        if key:
-            self.last_item = key
+        """Adds an item in the cache.
+
+        If the cache exceeds the maximum size, it discards
+        the last added item according to the LIFO policy.
+
+        Args:
+            key (str): The key under which the item is stored.
+            item (any): The item to store in the cache.
+        """
+        if key is None or item is None:
+            return
+
+        if key not in self.cache_data:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(last=True)
+                print("DISCARD:", last_key)
+
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
-        """ Get an item by key """
-        return self.cache_data.get(key)
+        """Retrieves an item by key.
+
+        Args:
+            key (str): The key of the item to retrieve.
+
+        Returns:
+            The item stored under the given key, or None if the key
+            does not exist.
+        """
+        return self.cache_data.get(key, None)
