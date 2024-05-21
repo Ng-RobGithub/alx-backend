a857232d7211824f74e0945adfbedf705719f150
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-22 00:03:38 +0100

    LFUCache Class

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -5,33 +5,36 @@ from collections import OrderedDict
 
 
 class LFUCache(BaseCaching):
-    """ LFU caching system """
-
+    """ LFU caching system
+    """
     def __init__(self):
         """ Initialize class instance. """
         super().__init__()
         self.cache_data = OrderedDict()
+        self.mru = ""
 
     def put(self, key, item):
-        """ Add an item in the cache """
-        if key is None or item is None:
-            return
-
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            if key in self.cache_data:
-                self.cache_data[key] = item
-                self.cache_data.move_to_end(key)
+        """ Add an item in the cache
+        """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if key in self.cache_data:
+                    self.cache_data.update({key: item})
+                    self.mru = key
+                else:
+                    # discard the most recently used item
+                    discarded = self.mru
+                    del self.cache_data[discarded]
+                    print("DISCARD: {}".format(discarded))
+                    self.cache_data[key] = item
+                    self.mru = key
             else:
-                # Discard the least frequently used item
-                discarded_key, _ = self.cache_data.popitem(last=False)
-                print("DISCARD:", discarded_key)
                 self.cache_data[key] = item
-        else:
-            self.cache_data[key] = item
+                self.mru = key
 
     def get(self, key):
-        """ Get an item by key """
+        """ Get an item by key
+        """
         if key in self.cache_data:
-            self.cache_data.move_to_end(key)
+            self.mru = key
             return self.cache_data[key]
-        return None
