9a230a79d4f83dd2db9ad65c3f6b175027b7a1a2
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-21 21:39:06 +0100

    LIFOCache class

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python3
-"""LIFO caching system"""
+"""Last-In First-Out caching module.
+"""
+from collections import OrderedDict
 
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """LIFOCache is a caching system that uses LIFO eviction policy"""
-
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a LIFO
+    removal mechanism when the limit is reached.
+    """
     def __init__(self):
-        """Initialize the class"""
+        """Initializes the cache.
+        """
         super().__init__()
-        self.stack = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Add an item to the cache"""
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                self.stack.remove(key)
-            self.cache_data[key] = item
-            self.stack.append(key)
-
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                last_key = self.stack.pop()
-                del self.cache_data[last_key]
-                print("DISCARD: {}".format(last_key))
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
-        """Get an item from the cache"""
+        """Retrieves an item by key.
+        """
         return self.cache_data.get(key, None)
