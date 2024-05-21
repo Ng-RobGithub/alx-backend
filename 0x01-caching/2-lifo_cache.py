a306b865acc7dca4a2f1f092492c81a43902f525
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-21 21:43:51 +0100

    LIFOCache class

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,35 +1,24 @@
-#!/usr/bin/env python3
-"""Last-In First-Out caching module.
-"""
-from collections import OrderedDict
-
+#!/usr/bin/python3
+""" Python caching systems """
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """Represents an object that allows storing and
-    retrieving items from a dictionary with a LIFO
-    removal mechanism when the limit is reached.
-    """
+    """ LIFO caching system """
     def __init__(self):
-        """Initializes the cache.
-        """
+        """ Initialize class instance. """
         super().__init__()
-        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Adds an item in the cache.
-        """
-        if key is None or item is None:
-            return
-        if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                last_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", last_key)
-        self.cache_data[key] = item
-        self.cache_data.move_to_end(key, last=True)
+        """ Add an item in the cache """
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            self.cache_data.pop(self.last_item)
+            print('DISCARD:', self.last_item)
+        if key:
+            self.last_item = key
 
     def get(self, key):
-        """Retrieves an item by key.
-        """
-        return self.cache_data.get(key, None)
+        """ Get an item by key """
+        return self.cache_data.get(key)
