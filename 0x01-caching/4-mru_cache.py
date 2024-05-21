d95739d71b179052931b59deeae4a13c4ca37a83
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-21 22:57:43 +0100

    MRUCache Class

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-""" MRU Cache Module """
-BaseCaching = __import__('base_caching').BaseCaching
+""" MRU caching """
+
+from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class MRUCache(BaseCaching):
-    """ MRU Cache class """
+    """ MRU cache class """
 
     def __init__(self):
-        """ Initialize MRU Cache """
+        """ Initialize MRU cache """
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item to the cache """
+        """ Add an item in the cache """
         if key is None or item is None:
             return
 
@@ -28,14 +31,12 @@ class MRUCache(BaseCaching):
         if key is None or key not in self.cache_data:
             return None
 
-        # Move the accessed item to the end of the cache_data
-        item = self.cache_data.pop(key)
-        self.cache_data[key] = item
-
-        return item
+        value = self.cache_data.pop(key)
+        self.cache_data[key] = value
+        return value
 
     def print_cache(self):
-        """ Print the cache """
+        """ Print cache content """
         print("Current cache:")
-        for k, v in self.cache_data.items():
-            print("{}: {}".format(k, v))
+        for key, value in self.cache_data.items():
+            print("{}: {}".format(key, value))
